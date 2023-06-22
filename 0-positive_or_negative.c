4acd40317d5b9751285cc9f2c4503fc38af7d42f
Beauty53 <beautymahlako@gmail.com>
2023-06-22 05:24:23 -0700

    The last digit

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,22 +1,22 @@
-#include <stdib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main - Dertermines if a number is positive, negative or zero.
+ * main - Determines if a number is positive, negative or zero.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success)	
  */
-int main(void)
-{
+int main(void)	
+{	
 	int n;
-
+		
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)	
-	{	
+	{		
 		print("%d is positive\n", n);
-	}	
+	}		
 	else if (n == 0)
 	{	
 		print("%d is zero\n", n);
@@ -24,6 +24,6 @@ int main(void)
 	else 
 	{
 		print("%d is negative\n", n);
-	}	
+	}		
 	return (0);
-}	
+}		
---
+++ 1-last_digit.c
@@ -0,0 +1,25 @@
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and another it is gtreater that 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ((n % 10) > 5)
+	{
+		printf( " Last digit of % d is % d and is greater than 5\n ",n n % 10);
+	}
+	if else ((n % 10) < 6 && (n % 10) !" 0)
+	{
+		printf( " Last digit of % d is % d and is greater than 6\n ",n n % 10);
+	else 
+	}
+		printf( " Last digit of % d is % d and is greater than 0\n ",n n % 10);
+	}
+	return(0);
+}
