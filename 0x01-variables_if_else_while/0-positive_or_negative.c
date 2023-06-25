8d5185398cb9961d858aa4d6559fcf9731a84618
Beauty53 <beautymahlako@gmail.com>
2023-06-25 12:33:33 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,25 +4,26 @@
 /**
  * main - Determine if number is negative, positive or zero.
  *
- * Retur: Always 0 (Success)
- */
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n-rand() - RAND_MAX / 2;
-	if(n>0)
+	n - rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
-		ptintf("%d is positive\n" , n);
+		printf("%d is positive\n", n);
 	}
-	else if (n=0)
+	else if (n = 0)
 	{
-		ptintf("%d is zero\n" , n);
+		printf("%d is zero\n", n);
 	}
 	else 
 	{
-		ptintf("%d is negative\n" , n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
