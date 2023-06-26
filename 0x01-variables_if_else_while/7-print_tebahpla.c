71f05be9762b487c721975c0631184a4d4f0e006
Beauty53 <beautymahlako@gmail.com>
2023-06-26 13:01:31 -0700

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,22 @@
-#include < stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 
-	char low;
-	for (low = 'z'; low >= 'a'; low--)
-	putchar(low);
-	putchar('\n');
+	char c;
 
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
 	return (0);
+
 }
